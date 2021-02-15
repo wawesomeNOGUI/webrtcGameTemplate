b7a51969edaf4f3e1947095175e74723236e397e
Will Forcey <wsforc3y@gmail.com>
2021-02-14 19:01:06 -0500

    Add files via upload

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"time"
-	"strconv"
-	"net/http"
 	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/websocket"
 
@@ -14,162 +14,158 @@ import (
 )
 
 var upgrader = websocket.Upgrader{
-							 		CheckOrigin: func(r *http.Request) bool { return true },
-							 }
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
 
 func echo(w http.ResponseWriter, r *http.Request) {
-        c, err := upgrader.Upgrade(w, r, nil)
-        if err != nil {
-                fmt.Println("Upgrade: ", err)
-                return
-        }
-        defer c.Close()
-				fmt.Println("User connected from: ", c.RemoteAddr())
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Upgrade: ", err)
+		return
+	}
+	defer c.Close()
+	fmt.Println("User connected from: ", c.RemoteAddr())
 
-				//===========WEBRTC====================================
-				// Prepare the configuration
-				config := webrtc.Configuration{
-					ICEServers: []webrtc.ICEServer{
-						{
-							URLs: []string{"stun:stun.l.google.com:19302"},
-						},
-					},
-				}
+	//===========WEBRTC====================================
+	// Prepare the configuration
+	config := webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{
+			{
+				URLs: []string{"stun:stun.l.google.com:19302"},
+			},
+		},
+	}
 
-				// Create a new RTCPeerConnection
-				peerConnection, err := webrtc.NewPeerConnection(config)
-				if err != nil {
-					panic(err)
-				}
+	// Create a new RTCPeerConnection
+	peerConnection, err := webrtc.NewPeerConnection(config)
+	if err != nil {
+		panic(err)
+	}
 
-				//Setup dataChannel to act like UDP (unordered, no retransmits) with the
-				//DataChannelInit struct
-				var udpPls webrtc.DataChannelInit
-				var retransmits uint16 = 0
-				var ordered = false
+	//Setup dataChannel to act like UDP (unordered, no retransmits) with the
+	//DataChannelInit struct
+	var udpPls webrtc.DataChannelInit
+	var retransmits uint16 = 0
+	var ordered = false
 
-				udpPls.Ordered = &ordered
-				udpPls.MaxRetransmits = &retransmits
+	udpPls.Ordered = &ordered
+	udpPls.MaxRetransmits = &retransmits
 
+	// Create a datachannel with label 'data' and options udpPls
+	dataChannel, err := peerConnection.CreateDataChannel("data", &udpPls)
+	if err != nil {
+		panic(err)
+	}
 
-				// Create a datachannel with label 'data' and options udpPls
-				dataChannel, err := peerConnection.CreateDataChannel("data", &udpPls)
-				if err != nil {
-					panic(err)
-				}
+	// Set the handler for ICE connection state
+	// This will notify you when the peer has connected/disconnected
+	peerConnection.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
+		fmt.Printf("ICE Connection State has changed: %s\n", connectionState.String())
+	})
 
+	// Register channel opening handling
+	dataChannel.OnOpen(func() {
+		fmt.Printf("Data channel '%s'-'%d' open. Random messages will now be sent to any connected DataChannels\n", dataChannel.Label(), dataChannel.ID())
 
+		var message int
 
-				// Set the handler for ICE connection state
-				// This will notify you when the peer has connected/disconnected
-				peerConnection.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
-					fmt.Printf("ICE Connection State has changed: %s\n", connectionState.String())
-				})
+		for {
+			time.Sleep(50) //50 nanoseconds
+			message++      //add 1 to message
+			//fmt.Printf("Sending '%s'\n", message)
 
-				// Register channel opening handling
-				dataChannel.OnOpen(func() {
-					fmt.Printf("Data channel '%s'-'%d' open. Random messages will now be sent to any connected DataChannels\n", dataChannel.Label(), dataChannel.ID())
+			// Send the message as text
+			sendErr := dataChannel.SendText(strconv.Itoa(message)) //make new byte slice with message as the only field
+			if sendErr != nil {
+				fmt.Println("data send err", sendErr)
+				break
+			}
+		}
 
-					var message int
+	})
 
-					for{
-						time.Sleep(50) //50 nanoseconds
-						message++ //add 1 to message
-						//fmt.Printf("Sending '%s'\n", message)
+	// Register text message handling
+	dataChannel.OnMessage(func(msg webrtc.DataChannelMessage) {
+		fmt.Printf("Message from DataChannel '%s': '%s'\n", dataChannel.Label(), string(msg.Data))
+	})
 
+	// Create an offer to send to the browser
+	offer, err := peerConnection.CreateOffer(nil)
+	if err != nil {
+		panic(err)
+	}
 
-						// Send the message as text
-						sendErr := dataChannel.SendText(strconv.Itoa(message))    //make new byte slice with message as the only field
-						if sendErr != nil {
-							fmt.Println("data send err", sendErr)
-							break
-						}
-					}
+	// Create channel that is blocked until ICE Gathering is complete
+	gatherComplete := webrtc.GatheringCompletePromise(peerConnection)
 
-				})
+	// Sets the LocalDescription, and starts our UDP listeners
+	err = peerConnection.SetLocalDescription(offer)
+	if err != nil {
+		panic(err)
+	}
 
-				// Register text message handling
-				dataChannel.OnMessage(func(msg webrtc.DataChannelMessage) {
-					fmt.Printf("Message from DataChannel '%s': '%s'\n", dataChannel.Label(), string(msg.Data))
-				})
+	// Block until ICE Gathering is complete, disabling trickle ICE
+	// we do this because we only can exchange one signaling message
+	// in a production application you should exchange ICE Candidates via OnICECandidate
+	<-gatherComplete
 
-				// Create an offer to send to the browser
-				offer, err := peerConnection.CreateOffer(nil)
-				if err != nil {
-					panic(err)
-				}
+	fmt.Println(*peerConnection.LocalDescription())
 
-				// Create channel that is blocked until ICE Gathering is complete
-				gatherComplete := webrtc.GatheringCompletePromise(peerConnection)
+	//Send the SDP with the final ICE candidate to the browser as our offer
+	err = c.WriteMessage(1, []byte(signal.Encode(*peerConnection.LocalDescription()))) //write message back to browser, 1 means message in byte format?
+	if err != nil {
+		fmt.Println("write:", err)
+	}
 
-				// Sets the LocalDescription, and starts our UDP listeners
-				err = peerConnection.SetLocalDescription(offer)
-				if err != nil {
-					panic(err)
-				}
+	//Wait for the browser to send an answer (its SDP)
+	msgType, message, err2 := c.ReadMessage() //ReadMessage blocks until message received
+	if err2 != nil {
+		fmt.Println("read:", err)
+	}
 
-				// Block until ICE Gathering is complete, disabling trickle ICE
-				// we do this because we only can exchange one signaling message
-				// in a production application you should exchange ICE Candidates via OnICECandidate
-				<-gatherComplete
+	answer := webrtc.SessionDescription{}
 
-				fmt.Println( *peerConnection.LocalDescription() )
+	signal.Decode(string(message), &answer) //set answer to the decoded SDP
+	fmt.Println(answer, msgType)
 
-				//Send the SDP with the final ICE candidate to the browser as our offer
-        err = c.WriteMessage(1, []byte(signal.Encode(*peerConnection.LocalDescription()))) //write message back to browser, 1 means message in byte format?
-        if err != nil {
-                fmt.Println("write:", err)
-        }
+	// Set the remote SessionDescription
+	err = peerConnection.SetRemoteDescription(answer)
+	if err != nil {
+		panic(err)
+	}
 
-				//Wait for the browser to send an answer (its SDP)
-        msgType, message, err2 := c.ReadMessage() //ReadMessage blocks until message received
-        if err2 != nil {
-                fmt.Println("read:", err)
-        }
+	//=====================Trickle ICE==============================================
+	//Make a new struct to use for trickle ICE candidates
+	var trickleCandidate webrtc.ICECandidateInit
+	var leftBracket uint8 = 123 //123 = ascii value of "{"
 
-				answer := webrtc.SessionDescription{}
+	for {
+		_, message, err2 := c.ReadMessage() //ReadMessage blocks until message received
+		if err2 != nil {
+			fmt.Println("read:", err)
+		}
 
-        signal.Decode(string(message), &answer) //set answer to the decoded SDP
-        fmt.Println(answer, msgType)
+		if message[0] == leftBracket {
+			err := json.Unmarshal(message, &trickleCandidate)
+			if err != nil {
+				fmt.Println("errorUnmarshal:", err)
+			}
 
-				// Set the remote SessionDescription
-        err = peerConnection.SetRemoteDescription(answer)
-        if err != nil {
-                panic(err)
-        }
+			fmt.Println(trickleCandidate)
 
-//=====================Trickle ICE==============================================
-				//Make a new struct to use for trickle ICE candidates
-				var trickleCandidate webrtc.ICECandidateInit
-				var leftBracket uint8 = 123   //123 = ascii value of "{"
+			err = peerConnection.AddICECandidate(trickleCandidate)
+			if err != nil {
+				fmt.Println("errorAddICE:", err)
+			}
+		}
 
-				for{
-					_, message, err2 := c.ReadMessage() //ReadMessage blocks until message received
-					if err2 != nil {
-									fmt.Println("read:", err)
-					}
-
-					if message[0] == leftBracket {
-							err := json.Unmarshal(message, &trickleCandidate)
-							if err != nil {
-								fmt.Println("errorUnmarshal:", err)
-							}
-
-							fmt.Println(trickleCandidate)
-
-							err = peerConnection.AddICECandidate(trickleCandidate)
-							if err != nil {
-								fmt.Println("errorAddICE:", err)
-							}
-					}
-
-				}
+	}
 
 }
-				//WEBRTC connection made!
+
+//WEBRTC connection made!
 
 func main() {
-
 
 	fileServer := http.FileServer(http.Dir("./public"))
 	http.HandleFunc("/echo", echo) //this request comes from webrtc.html
@@ -177,6 +173,6 @@ func main() {
 
 	err := http.ListenAndServe(":80", nil) //Http server blocks
 	if err != nil {
-					panic(err)
+		panic(err)
 	}
 }
