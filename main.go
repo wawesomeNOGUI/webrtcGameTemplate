0a4d1b21202c6689ce82d083d5505ea58cc5afa3
Will Forcey <wsforc3y@gmail.com>
2021-04-28 19:44:27 -0400

    Add files via upload

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net"
 	"net/http"
 	"strconv"
 	"sync"
@@ -20,6 +21,8 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
+var api *webrtc.API
+
 func echo(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -33,17 +36,8 @@ func echo(w http.ResponseWriter, r *http.Request) {
 	var playerTag string
 
 	//===========WEBRTC====================================
-	// Prepare the configuration
-	config := webrtc.Configuration{
-		ICEServers: []webrtc.ICEServer{
-			{
-				URLs: []string{"stun:stun.l.google.com:19302"},
-			},
-		},
-	}
-
 	// Create a new RTCPeerConnection
-	peerConnection, err := webrtc.NewPeerConnection(config)
+	peerConnection, err := api.NewPeerConnection(webrtc.Configuration{})
 	if err != nil {
 		panic(err)
 	}
@@ -107,8 +101,6 @@ func echo(w http.ResponseWriter, r *http.Request) {
 	//====================No retransmits, ordered dataChannel=======================
 	// Register channel opening handling
 	dataChannel.OnOpen(func() {
-		fmt.Printf("Data channel '%s'-'%d' open. Random messages will now be sent to any connected DataChannels\n", dataChannel.Label(), dataChannel.ID())
-
 		for {
 			time.Sleep(time.Millisecond * 50) //50 milliseconds = 20 updates per second
 			//20 milliseconds = ~60 updates per second
@@ -287,11 +279,37 @@ func main() {
 
 	go getSyncMapReadyForSending(&Updates)
 
+	// Listen on UDP Port 80, will be used for all WebRTC traffic
+	udpListener, err := net.ListenUDP("udp", &net.UDPAddr{
+		IP:   net.IP{0, 0, 0, 0},
+		Port: 80,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Listening for WebRTC traffic at %s\n", udpListener.LocalAddr())
+
+	// Create a SettingEngine, this allows non-standard WebRTC behavior
+	settingEngine := webrtc.SettingEngine{}
+
+	//Our Public Candidate is declared here cause were not using a STUN server for discovery
+	//and just hardcoding the open port, and port forwarding webrtc traffic on the router
+	settingEngine.SetNAT1To1IPs([]string{"162.200.58.171"}, webrtc.ICECandidateTypeHost)
+
+	// Configure our SettingEngine to use our UDPMux. By default a PeerConnection has
+	// no global state. The API+SettingEngine allows the user to share state between them.
+	// In this case we are sharing our listening port across many.
+	settingEngine.SetICEUDPMux(webrtc.NewICEUDPMux(nil, udpListener))
+
+	// Create a new API using our SettingEngine
+	api = webrtc.NewAPI(webrtc.WithSettingEngine(settingEngine))
+
 	fileServer := http.FileServer(http.Dir("./public"))
 	http.HandleFunc("/echo", echo) //this request comes from webrtc.html
 	http.Handle("/", fileServer)
 
-	err := http.ListenAndServe(":80", nil) //Http server blocks
+	err = http.ListenAndServe(":80", nil) //Http server blocks
 	if err != nil {
 		panic(err)
 	}
